5a4032213c1657dde91d0f6ca2f6ddec42481a76
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2016-06-24 10:25:09 +0200

    Start event infrastructure to control UI

---
+++ src/main/java/fr/cnrs/ibmp/NewFigureEvent.java
@@ -0,0 +1,11 @@
+package fr.cnrs.ibmp;
+
+import java.util.EventObject;
+
+public class NewFigureEvent extends EventObject {
+
+	public NewFigureEvent(Object source) {
+		super(source);
+	}
+
+}
---
+++ src/main/java/fr/cnrs/ibmp/NewFigureListener.java
@@ -0,0 +1,14 @@
+package fr.cnrs.ibmp;
+
+import java.util.EventListener;
+
+/**
+ * TODO Documentation
+ * 
+ * @author Stefan Helfrich
+ */
+public interface NewFigureListener extends EventListener {
+
+	void newFigure(NewFigureEvent e);
+
+}
