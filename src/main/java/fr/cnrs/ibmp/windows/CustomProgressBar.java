054bafada9e3554f44d202072d747ac228a22d00
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2016-08-04 09:25:13 +0200

    CustomProgressBar: add javadoc

--- src/main/java/fr/cnrs/ibmp/windows/CustomProgressBar.java
+++ src/main/java/fr/cnrs/ibmp/windows/CustomProgressBar.java
@@ -1,10 +1,19 @@
-// TODO Missing license header
+
 package fr.cnrs.ibmp.windows;
 
+import fr.cnrs.ibmp.treeMap.LeafPanel;
 import ij.IJ;
 
 /**
- * TODO Documentation
+ * Customized progress bar to show the status of the transfer of information
+ * from an image to a {@link LeafPanel}. It uses the ImageJ status bar to show
+ * the progress.
+ * <p>
+ * (c) IBMP-CNRS
+ * </p>
+ * 
+ * @author Edda Zinck
+ * @author Jerome Mutterer
  */
 public class CustomProgressBar implements Runnable {
 
@@ -15,11 +24,12 @@ public class CustomProgressBar implements Runnable {
 			int i = 0;
 			while (true) {
 				i = (i + 1) % 12;
-				IJ.showStatus("Scaling image. " + s.substring(0, i) + "***"
-						+ s.substring(0, 12 - i));
+				IJ.showStatus("Scaling image. " + s.substring(0, i) + "***" + s
+					.substring(0, 12 - i));
 				Thread.sleep(100);
 			}
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			// TODO Handle exception
 		}
 	}
